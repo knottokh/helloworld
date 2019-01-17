5c8fb6c36f8e561d826a6ecadfc2ce1fefb58f16
Knotto <one2treego@hotmail.com>
2019-01-17 13:49:28 +0000

    Set up Puma, Nginx & Capistrano 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,8 @@ set :repo_url,        'git@github.com:knottokh/helloworld.git'
 set :application,     'helloworld'
 set :user,            'rails'
 set :password,         'f858222cc6b7d1fbbdd0f8b1b6c5f33a'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+#set :puma_threads,    [4, 16]
+#set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -20,15 +20,15 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+#set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+#set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+#set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+#set :puma_access_log, "#{release_path}/log/puma.error.log"
+#set :puma_error_log,  "#{release_path}/log/puma.access.log"
+#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+#set :puma_preload_app, true
+#set :puma_worker_timeout, nil
+#set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -41,17 +41,6 @@ set :puma_init_active_record, false  # Change to true if using ActiveRecord
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
