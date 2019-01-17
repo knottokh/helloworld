cf2791b6504b8c6a84af11374fcfc62bfb6893ff
Knotto <one2treego@hotmail.com>
2019-01-17 17:26:53 +0000

    Set up Puma, Nginx & Capistrano 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '128.199.104.195', port: 80, roles: [:web, :app, :db], primary: true
+server '128.199.104.195', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@example.com:knottokh/helloworld.git'
 set :application,     'helloworld'
